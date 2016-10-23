1383b84775dcb2d3ddc850868b2744944d15636f
LilyCole <Lilia.Cole@gmail.com>
2016-10-23 13:42:08 -0700

    changes to postgresql

--- db/migrate/20161022233634_create_superhero_superpowers.rb
+++ db/migrate/20161022233634_create_superhero_superpowers.rb
@@ -1,5 +1,7 @@
 class CreateSuperheroSuperpowers < ActiveRecord::Migration[5.0]
   def change
+    rename_table :superheros, :superheroes
+    
     create_table :superhero_superpowers do |t|
       t.belongs_to :superhero, foreign_key: true
       t.belongs_to :superpower, foreign_key: true
